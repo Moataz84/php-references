ef97ba8a83e324cf28d31304033f88df51a04f13
Moataz Ghazy <75513615+Moataz84@users.noreply.github.com>
2024-02-25 23:34:52 -0500

    Add files via upload

--- api/add-post.php
+++ api/add-post.php
@@ -1,1 +1,19 @@
+<?php 
+  include '../inc/middleware.php';
+  include '../inc/database.php';
 
+  $handler = function() {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $id = Null;
+    $name = $data['name'];
+    $email = $data['email'];
+    $comment = $data['message'];
+    $posted = date('Y-m-d');
+
+    $stmt = $GLOBALS['connection'] -> prepare("INSERT INTO comments(id, name, email, comment, posted) VALUES (?, ?, ?, ?, ?)");
+    $stmt -> bind_param("issss", $id, $name, $email, $comment, $posted);
+    $stmt -> execute();
+  };
+
+  checkOrigin($handler);
+?>
